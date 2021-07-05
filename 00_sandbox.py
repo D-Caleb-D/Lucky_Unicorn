9b25739208f95b403b0f69f1957634b95215900d
penney.caleb <penney.caleb@awatapu.school.nz>
2021-07-05 13:21:15 +1200

    i watch video and complete code

---
+++ 00_sandbox.py
@@ -0,0 +1,11 @@
+# Ask user to input a number
+get_number = int(input("Choose a number?"))
+# Multiply by 5
+times_five = get_number * 5
+# Output the result
+answer = ("{} times five is equal to " \
+      "{}" .format(get_number, times_five))
+
+# Output the result
+print(answer)
+
