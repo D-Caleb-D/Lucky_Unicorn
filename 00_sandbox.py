b51c5d19bcfa9ee994851cf0d11b25f418480e68
penney.caleb <penney.caleb@awatapu.school.nz>
2021-07-06 14:32:26 +1200

    changed it to .lower() so it made sure it was xxx not XXX

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,11 +1,9 @@
-# Ask user to input a number
-get_number = int(input("Choose a number?"))
-# Multiply by 5
-times_five = get_number * 5
-# Output the result
-answer = ("{} times five is equal to " \
-      "{}" .format(get_number, times_five))
+# get name until an exit code is entered...
 
-# Output the result
-print(answer)
+name = ""
+while name.lower() != "xxx":
+      name= input("Who are you?")
+      print(name)
 
+print()
+print("We are done!")
